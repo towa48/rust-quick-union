8ece83e679ccc0774a81847d1ceaab4089fa44a6
Anton Prilepsky <towa48@gmail.com>
2020-02-08 22:08:59 +0300

    Implement algorithm

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,55 @@ struct AppConfig {
     unions: Vec<Union>,
 }
 
+struct Calculator {
+    state: Vec<u8>,
+}
+
+impl Calculator {
+    fn new(size: u8) -> Calculator {
+        let mut result = Calculator {
+            state: vec![0; size as usize],
+        };
+
+        for i in 0..size {
+            result.state[i as usize] = i;
+        }
+
+        return result;
+    }
+
+    fn find_root(&mut self, index: u8) -> u8 {
+        let mut i = index;
+        while i != self.state[i as usize] {
+            i = self.state[i as usize];
+        }
+
+        return i;
+    }
+
+    fn union(&mut self, node_a: u8, node_b: u8) -> () {
+        let root_a = self.find_root(node_a);
+        let root_b = self.find_root(node_b);
+
+        self.state[root_a as usize] = root_b;
+    }
+
+    fn is_connected(&mut self, node_a: u8, node_b: u8) -> bool {
+        return self.find_root(node_a) == self.find_root(node_b);
+    }
+}
+
 fn main() {
     println!("Quick Union Example");
 
     let raw_config = fs::read_to_string("input.json").unwrap();
     let config: AppConfig = serde_json::from_str(&raw_config).unwrap();
 
-    println!("Size: {}", config.size);
+    let mut calculator = Calculator::new(config.size);
+    for (_i, pair) in config.unions.iter().enumerate() {
+        calculator.union(pair.from, pair.to);
+    }
+
+    let connected = calculator.is_connected(2, 13);
+    println!("2 and 13 connected: {}", connected);
 }
